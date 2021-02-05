05f2f64613897d4ad236b3c6538093e8e7ba3d96
Drucilla Deroche <drucimimi@protonmail.com>
2021-02-05 15:51:56 +0100

    Update files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 $(function(){
-    var $mainMenuItems = $("#main-menu ul").children("li"), //éléments du main-menu
+    // sélectionner l'image de chaque actrice
+    // et voir la description de la 3e actrice au lancement de la page
+    var $mainMenuItems = $("#main-menu ul").children("li"),
         totalMainMenuItems = $mainMenuItems.length,
         openedIndex = 2,
 
+        //Si l'index de l'image est valide, on peut voir la description de l'actrice sélectionnée
         init = function(){
           bindEvents();
           if(validIndex(openedIndex)){
@@ -16,6 +19,7 @@ $(function(){
             checkAndAnimateItem(newIndex);
           });
 
+          //appliquer un nouveau style quand on passe au-dessus d'un bouton
           $(".button").hover(
             function(){
               $(this).addClass("hovered");
@@ -25,6 +29,7 @@ $(function(){
             }
         );
 
+        //quand on clique sur un bouton au nom de l'actrice, on peut voir la description de celle-ci
         $(".button").click(function(){
           var newIndex = $(this).index();
           checkAndAnimateItem(newIndex);
@@ -36,14 +41,17 @@ $(function(){
         return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
       },
 
+      //quand on clique sur l'image d'actrice, elle est en couleur
       animateItem = function($item, toOpen, speed){
-        var $colorImage = $item.find(".color"), //image bw devient en couleur
+        var $colorImage = $item.find(".color"),
         itemParam = toOpen ? {width: "420px"} : {width: "140px"}, //toOpen est vrai prend une largeur de 420px sinon prend une largeur de 140px
         colorImageParam = toOpen ? {left: "0px"} : {left: "140px"};
         $colorImage.animate(colorImageParam, speed); //image en couleur superposé sur image bw
         $item.animate(itemParam , speed); //largeur de l'image augmentée
       },
 
+      //quand on clique sur une autre image, la description de l'image pré-sélectionnée disparaît
+      //et celle de l'image cliquée apparaît
       checkAndAnimateItem = function(indexToCheckAndAnimate){
         if(openedIndex === indexToCheckAndAnimate){
           animateItem($mainMenuItems.eq(indexToCheckAndAnimate), false, 250);
